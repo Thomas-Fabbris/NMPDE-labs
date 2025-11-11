8472d623b650c9d4eb1a6ac1a35f4038143065a7
Thomas Fabbris <thomasfabbris9@gmail.com>
2025-11-11 23:04:20 +0100

    lab-00-notes

--- lab-01/src/Poisson1D.cpp
+++ lab-01/src/Poisson1D.cpp
@@ -1,17 +1,37 @@
 #include "Poisson1D.hpp"
 
-void
-Poisson1D::setup()
-{}
+void Poisson1D::setup() {
+  // Create the mesh (through dealII functions for simple domains)
+  // colorize = true assignis
+  GridGenerator::subdivided_hyper_cube(mesh, N_el, 0.0, 1.0,
+                                       /* colorize = */ true);
 
-void
-Poisson1D::assemble()
-{}
+  std::cout << " Number of elements: " << mesh.n_active_cells() << std::endl;
 
-void
-Poisson1D::solve()
-{}
+  // Initialize the finite element space
+  fe = std::make_unique<FE_SimplexP<dim>>(r);
+  quadrature = std::make_unique<QGaussSimplex<dim>>(r + 1);
 
-void
-Poisson1D::output() const
-{}
+  std::cout << " DoF per cell: " << fe->dofs_per_cell << std::endl;
+
+  dof_handler.reinit(mesh);
+  dof_handler.distribute_dofs(*fe);
+
+  std::cout << " Number of DoFs: " << dof_handler.n_dofs << std::endl;
+
+  // Initialize linear algebra
+  DynamicSparsityPattern dsp(dof_handler.n_dofs());
+  DoFTools::make_sparsity_pattern(dof_handler, dsp);
+  sparsity_pattern_copy_from(dsp);
+
+  system_matrix.reinit(sparsity_pattern);
+
+  system_rhs.reinit(dof_handler.n_dofs());
+  solution.reinit(dof_handler.n_dofs());
+}
+
+void Poisson1D::assemble() {}
+
+void Poisson1D::solve() {}
+
+void Poisson1D::output() const {}
--- lab-01/src/Poisson1D.hpp
+++ lab-01/src/Poisson1D.hpp
@@ -2,23 +2,18 @@
 #define POISSON_1D_HPP
 
 #include <deal.II/base/quadrature_lib.h>
-
 #include <deal.II/dofs/dof_handler.h>
 #include <deal.II/dofs/dof_tools.h>
-
 #include <deal.II/fe/fe_simplex_p.h>
 #include <deal.II/fe/fe_values.h>
-
 #include <deal.II/grid/grid_generator.h>
 #include <deal.II/grid/grid_out.h>
 #include <deal.II/grid/tria.h>
-
 #include <deal.II/lac/dynamic_sparsity_pattern.h>
 #include <deal.II/lac/precondition.h>
 #include <deal.II/lac/solver_cg.h>
 #include <deal.II/lac/sparse_matrix.h>
 #include <deal.II/lac/vector.h>
-
 #include <deal.II/numerics/data_out.h>
 #include <deal.II/numerics/matrix_tools.h>
 #include <deal.II/numerics/vector_tools.h>
@@ -31,40 +26,30 @@ using namespace dealii;
 /**
  * Class managing the differential problem.
  */
-class Poisson1D
-{
-public:
+class Poisson1D {
+ public:
   // Physical dimension (1D, 2D, 3D)
   static constexpr unsigned int dim = 1;
 
   // Constructor.
-  Poisson1D(const unsigned int                              &N_el_,
-            const unsigned int                              &r_,
-            const std::function<double(const Point<dim> &)> &mu_,
-            const std::function<double(const Point<dim> &)> &f_)
-    : N_el(N_el_)
-    , r(r_)
-    , mu(mu_)
-    , f(f_)
-  {}
+  Poisson1D(const unsigned int& N_el_, const unsigned int& r_,
+            const std::function<double(const Point<dim>&)>& mu_,
+            const std::function<double(const Point<dim>&)>& f_)
+      : N_el(N_el_), r(r_), mu(mu_), f(f_) {}
 
   // Initialization.
-  void
-  setup();
+  void setup();
 
   // System assembly.
-  void
-  assemble();
+  void assemble();
 
   // System solution.
-  void
-  solve();
+  void solve();
 
-  // Output.
-  void
-  output() const;
+  // Output (write result to output files)
+  void output() const;
 
-protected:
+ protected:
   // Number of elements.
   const unsigned int N_el;
 
@@ -72,10 +57,27 @@ protected:
   const unsigned int r;
 
   // Diffusion coefficient.
-  std::function<double(const Point<dim> &)> mu;
+  std::function<double(const Point<dim>&)> mu;
 
   // Forcing term.
-  std::function<double(const Point<dim> &)> f;
+  std::function<double(const Point<dim>&)> f;
+
+  // Mesh.
+  Triangulation<dim> mesh;
+
+  // Finite element space.
+  std::unique_pointer<FiniteElement<dim>> fe;
+
+  // Quadrature rule.
+  std::unique_pointer<Quadrature<dim>> quadrature;
+
+  DoFHandler<dim> dof_handler;
+
+  SparsityPattern sparsity_pattern;
+  SparseMatrix<double> system_matrix;
+
+  Vector<double> system_rhs;
+  Vector<double> solution;
 };
 
 #endif
